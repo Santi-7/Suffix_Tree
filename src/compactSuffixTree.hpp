d8c7b2e3fc478d44c71ad32d8835a192764c8fd6
Santi Gil <santi.gil.begue7@gmail.com>
2017-01-20 20:48:54 +0100

    Define a (compact) suffix tree

--- src/compactSuffixTree.hpp
+++ src/compactSuffixTree.hpp
@@ -9,9 +9,31 @@
 #ifndef SUFFIX_TREE_COMPACTSUFFIXTREE_HPP
 #define SUFFIX_TREE_COMPACTSUFFIXTREE_HPP
 
+#include <string>
+#include <vector>
+
+struct TreeNode
+{
+    // An edge is a string and a pointer to another node.
+    std::vector<std::pair(std::string, TreeNode *)> edges;
+};
+
 class CompactSuffixTree
 {
 
+public:
+
+    /**
+     * TODO: Add doc.
+     * @param str
+     * @return
+     */
+    CompactSuffixTree(const std::string &str);
+
+private:
+
+    /** Root node of the compact suffix tree. */
+    TreeNode mRootNode;
 };
 
 #endif // SUFFIX_TREE_COMPACTSUFFIXTREE_HPP
--- src/suffixTree.hpp
+++ src/suffixTree.hpp
@@ -9,9 +9,31 @@
 #ifndef SUFFIX_TREE_SUFFIXTREE_HPP
 #define SUFFIX_TREE_SUFFIXTREE_HPP
 
+#include <string>
+#include <vector>
+
+struct TreeNode
+{
+    // An edge is a char and a pointer to another node.
+    std::vector<std::pair(char, TreeNode*)> edges;
+};
+
 class SuffixTree
 {
 
+public:
+
+    /**
+     * TODO: Add doc.
+     * @param str
+     * @return
+     */
+    SuffixTree(const std::string &str);
+
+private:
+
+    /** Root node of the suffix tree. */
+    TreeNode mRootNode;
 };
 
 #endif // SUFFIX_TREE_SUFFIXTREE_HPP
